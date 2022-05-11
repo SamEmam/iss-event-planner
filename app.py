56a693d7d75cff439c99d88f7080a28c40a97a86
SamEmam <samhoffmann111@hotmail.com>
2022-05-11 21:20:00 +0200

    ready for deployment

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import json
 import os
 
-from flask import request, jsonify, Flask, render_template
-from injector import inject
+from flask import request, Flask, render_template, redirect, url_for
+
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+data_path = "/data/"
+data_file = os.path.join(SITE_ROOT, data_path, "event_data.json")
+# data_file = "event_data.json"
 
 
 def config_app(app):
     app.config["DEBUG"] = True
+    app.config['SECRET_KEY'] = "hest1234"
 
 
 def create_app() -> Flask:
@@ -13,34 +20,48 @@ def create_app() -> Flask:
 
     config_app(app)
 
-
-    @app.route('/', methods=['GET'])
+    @app.route('/', methods=['GET', 'POST'])
     def index():
-        return render_template('index.html')
+        data = json.load(open(data_file, 'r'))
+        data = sorted(data, key=lambda d: d['date'])
 
+        if request.method == 'POST':
+            if request.form['input_button'] == 'Create event':
+                input_host = request.form['input_host']
+                input_months = request.form['input_months']
+                input_date = request.form['input_date']
+                input_desc = request.form['input_desc']
+                data.append({
+                    "host": input_host,
+                    "months": input_months,
+                    "date": input_date,
+                    "description": input_desc
+                })
 
-    @inject
-    @app.route('/ui/womf', methods=['GET'])
-    def womf_home():
+                json.dump(data, open(data_file, 'w'))
+                return redirect(url_for('index'))
 
-        incident = servicenow_incident['number']['displayValue']
-        title = servicenow_incident['u_short_description']['displayValue']
-        description = cleanup_html(servicenow_incident['u_description']['displayValue'])
-        resolution = cleanup_html(servicenow_incident['u_resolution_description']['displayValue'])
-        date = servicenow_incident['resolved_at']['displayValue']
-        users_affected = servicenow_incident['impact']['displayValue']
-        response_dur = servicenow_incident['u_downtime_duration']['displayValue']
+            elif request.form['input_button'] == 'Delete':
+                input_index = int(request.form['input_index'])
+                del data[input_index]
+
+                json.dump(data, open(data_file, 'w'))
+                return redirect(url_for('index'))
+
+            elif request.form['input_button'] == 'Save':
+                input_index = int(request.form['input_index'])
+                input_date = request.form['input_date']
+                input_desc = request.form['input_desc']
+
+                data[input_index]['date'] = input_date
+                data[input_index]['description'] = input_desc
+
+                json.dump(data, open(data_file, 'w'))
+                return redirect(url_for('index'))
 
         return render_template(
-            "womf_home.html",
-            incident=incident,
-            title=title,
-            description=description,
-            resolution=resolution,
-            date=date,
-            users_affected=users_affected,
-            response_dur=response_dur,
-            appkey=request.args.get('key'),
+            'index.html',
+            data=data
         )
 
     return app
