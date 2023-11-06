7b8b9eba8367a0275befe6fcdf2106d07893ca49
SamEmam <shb@energinet.dk>
2023-11-06 12:21:52 +0100

    ensure dnd ics is correct

--- scripts/cron-auto-events/cron-new-events.py
+++ scripts/cron-auto-events/cron-new-events.py
@@ -178,8 +178,9 @@ def generate_ics_file(input_file, output_file, type):
             cal.events.add(create_ical_event_padel(event_data))
     elif type == dnd_enum:
         for event_data in data:
-            if (event_data['votes'] + event_data['votes_indeterminate']) > 3:
-                cal.events.add(create_ical_event_dnd(event_data))
+            # if (event_data['votes'] + event_data['votes_indeterminate']) > 3:
+            #     cal.events.add(create_ical_event_dnd(event_data))
+            cal.events.add(create_ical_event_dnd(event_data))
     else:
         return
 
